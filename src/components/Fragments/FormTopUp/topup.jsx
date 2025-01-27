cfa3676999cccaa9154907200d4849f2855622ab
Pongo <echotech8310@gmail.com>
2025-01-27 23:27:03 +0700

    update validating topup

--- src/components/Fragments/FormTopUp/topup.jsx
+++ src/components/Fragments/FormTopUp/topup.jsx
@@ -3,6 +3,10 @@ import {toast} from 'react-toastify'
 async function Topup(e) {
     e.preventDefault()
     const nominal = e.target.topup.value
+
+    if(nominal < 10000 || nominal > 1000000) {
+        toast.error('Nominal Tidak Sesuai',{position: "bottom-right",autoClose: 3000,hideProgressBar: false,closeOnClick: false,pauseOnHover: true,draggable: true,progress: undefined,theme: "colored"})
+    }
     const data = await TopUp(parseInt(nominal))
     toast.success(data.message,{position: "bottom-right",autoClose: 3000,hideProgressBar: false,closeOnClick: false,pauseOnHover: true,draggable: true,progress: undefined,theme: "colored",onClose: () => {
         window.location.href = '/transaction'
